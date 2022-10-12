f9c0e7eeb2b9d40cf6bd543604083ac862dbfc12
zaragozabraham <abrahamezg@gmail.com>
2022-10-12 12:21:58 -0500

    Add update & create routes for posts, Add Posts post/update tests, Add update & create methods for PostsController, Add  rescue exeptions to PostsController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,47 @@
 # frozen_string_literal: true
 
 class PostsController < ApplicationController
+  rescue_from Exception do |e|
+    log.error e.message.to_s
+    render json: { error: e.message }, status: :internal_error
+  end
+
+  rescue_from ActiveRecord::RecordInvalid do |e|
+    render json: { error: e.message }, status: :unprocessable_entity
+  end
+
+  before_action :set_post, only: %i[show update]
+
   def index
     @posts = Post.where(published: true)
     render json: @posts, status: :ok
   end
 
   def show
-    @post = Post.find(params[:id])
     render json: @post, status: :ok
+  end
+
+  def create
+    @post = Post.create!(create_post_params)
+    render json: @post, status: :created
+  end
+
+  def update
+    @post.update!(update_post_params)
+    render json: @post, status: :ok
+  end
+
+  private
+
+  def create_post_params
+    params.require(:post).permit(%i[title content published user_id])
+  end
+
+  def update_post_params
+    params.require(:post).permit(%i[title content published])
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
--- spec/requests/post_spec.rb
+++ spec/requests/post_spec.rb
@@ -38,4 +38,70 @@ RSpec.describe 'Posts', type: :request do
       expect(payload['id']).to eq(post.id)
     end
   end
+
+  describe 'POST /posts' do
+    let!(:user) { create(:user) }
+    let(:payload) { JSON.parse(response.body) }
+    let(:req_payload) do
+      { post: {
+        title: 'some title',
+        content: 'some content of the post',
+        published: false,
+        user_id: user.id
+      } }
+    end
+
+    before { post '/posts', params: req_payload }
+
+    it 'creates a post' do
+      expect(payload['id']).not_to be_nil
+    end
+
+    it 'creates a post and returns status created' do
+      expect(response).to have_http_status(:created)
+    end
+  end
+
+  describe 'POST /posts with invalid post' do
+    let!(:user) { create(:user) }
+    let(:payload) { JSON.parse(response.body) }
+    let(:req_payload) do
+      { post: {
+        content: 'some content of the post',
+        published: false,
+        user_id: user.id
+      } }
+    end
+
+    before { post '/posts', params: req_payload }
+
+    it 'returns error message on invalid post' do
+      expect(payload['error']).not_to be_empty
+    end
+
+    it 'returns returns unprocessable entity status code' do
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+  end
+
+  describe 'PUT /posts/:id' do
+    let!(:article) { create(:post) }
+    let(:payload) { JSON.parse(response.body) }
+    let(:req_payload) do
+      { post: {
+        title: 'some title',
+        published: false
+      } }
+    end
+
+    before { put "/posts/#{article.id}", params: req_payload }
+
+    it 'creates a post, updates it and return status code 200' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "creates a post and updates its title to 'some title'" do
+      expect(payload['title']).to eq('some title')
+    end
+  end
 end
